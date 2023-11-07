b4c0fb2ac95a9364c81fb92db827a2011ca7d8c5
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-11-07 00:57:30 +0000

    fully functioning birthday countdown
    counts down time in seconds

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const timeLeft = document.getElementById('time-left')
 
-const today = new Date()
+const birthday = new Date('11/10/2023')
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24 // milliseconds
+let timerID
 
-console.log(today)
+function countDown() {
+  const today = new Date()
+  const timeSpan = birthday - today
+
+  if(timeSpan <=-day){
+    timeLeft.innerHTML = "Hope you had a nice Birtday!"
+    clearInterval(timerId)
+    return
+  }
+  if(timeSpan <= 0) {
+    timeLeft.innerHTML = "Happy Birthday"
+    clearInterval()
+    return
+  }
+
+  const days = Math.floor( timeSpan/ day )
+  const hours = Math.floor((timeSpan % day) / hour)
+  const minutes = Math.floor((timeSpan % hour) / minute)
+  const seconds = Math.floor((timeSpan % minute) / second)
+
+  timeLeft.innerHTML = days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds'
+}
+
+timerId=setInterval(countDown, second)
